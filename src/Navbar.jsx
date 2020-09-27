b088b6975ad4104cd119a425a210a1ae4877c442
sudan <amstersudhan633@gmail.com>
2020-09-27 21:10:31 +0545

    Version 3

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -16,32 +16,21 @@ const Navbar = () => {
                                 <span className="navbar-toggler-icon"></span>
                             </button>
                             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                                <ul className="navbar-nav mr-auto mb-2 mb-lg-0">
+                                <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
                                     <li className="nav-item">
                                         <a className="nav-link active" aria-current="page" href="#">Home</a>
                                     </li>
                                     <li className="nav-item">
-                                        <a className="nav-link" href="#">Link</a>
-                                    </li>
-                                    <li className="nav-item dropdown">
-                                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
-                                            Dropdown
-          </a>
-                                        <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                            <li><a className="dropdown-item" href="#">Action</a></li>
-                                            <li><a className="dropdown-item" href="#">Another action</a></li>
-                                            <li><hr className="dropdown-divider" /></li>
-                                            <li><a className="dropdown-item" href="#">Something else here</a></li>
-                                        </ul>
+                                        <a className="nav-link" href="#">Services</a>
                                     </li>
                                     <li className="nav-item">
-                                        <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                                        <a className="nav-link" href="#">About Us</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#">COntact Us</a>
                                     </li>
                                 </ul>
-                                <form className="d-flex">
-                                    <input className="form-control mr-2" type="search" placeholder="Search" aria-label="Search" />
-                                    <button className="btn btn-outline-success" type="submit">Search</button>
-                                </form>
+
                             </div>
                         </div>
                     </nav>
