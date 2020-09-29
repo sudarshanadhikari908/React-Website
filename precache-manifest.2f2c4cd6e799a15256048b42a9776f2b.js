d1faa35ed49c32f8230477c622e9efd37df21f17
sudan <amstersudhan633@gmail.com>
2020-09-29 13:35:15 +0545

    Updates

---
+++ precache-manifest.2f2c4cd6e799a15256048b42a9776f2b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6c24c6b89a6c64418a96b26878a999a",
+    "url": "/React-Website/index.html"
+  },
+  {
+    "revision": "8a23a0f282ed19312348",
+    "url": "/React-Website/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "94bbcfa8ab9c53988905",
+    "url": "/React-Website/static/css/main.eec05be6.chunk.css"
+  },
+  {
+    "revision": "8a23a0f282ed19312348",
+    "url": "/React-Website/static/js/2.1d4299ca.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/React-Website/static/js/2.1d4299ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94bbcfa8ab9c53988905",
+    "url": "/React-Website/static/js/main.92af30da.chunk.js"
+  },
+  {
+    "revision": "c560040dd0c3437c4843",
+    "url": "/React-Website/static/js/runtime-main.279a2ae7.js"
+  },
+  {
+    "revision": "7bb36491e657164a0cbbf145568e86c1",
+    "url": "/React-Website/static/media/1.7bb36491.jpg"
+  },
+  {
+    "revision": "58edf65baedd8be7730a1bde42f6f16b",
+    "url": "/React-Website/static/media/app.58edf65b.jpg"
+  },
+  {
+    "revision": "6c6957db2f5c24973dfd6dc3f1b8d984",
+    "url": "/React-Website/static/media/digital.6c6957db.jpg"
+  },
+  {
+    "revision": "f6fec884926232e95a2ddbaf87944dcc",
+    "url": "/React-Website/static/media/marketing.f6fec884.jpg"
+  },
+  {
+    "revision": "0965a300e6b8e2f87d6869a42145107e",
+    "url": "/React-Website/static/media/software.0965a300.jpg"
+  },
+  {
+    "revision": "dc6a1f7813673ea185b2d6889788226f",
+    "url": "/React-Website/static/media/web.dc6a1f78.jpg"
+  }
+]);
